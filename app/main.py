11815419365f0efa2e8588dd3092d3e246778ae5
solomonadzape95 <adzapesolomon@gmail.com>
2026-03-10 22:46:06 +0100

    added CORS support for the GDG website

--- app/main.py
+++ app/main.py
@@ -24,9 +24,10 @@ app = FastAPI(title="Google Developer Group on Campus, UNN Community API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "http://127.0.0.1:3000",
-                 "http://localhost:5173",
-                 "http://127.0.0.1:5173"],  
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://gdg-website-llgf.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
